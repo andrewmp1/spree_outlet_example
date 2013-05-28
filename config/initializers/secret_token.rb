aa618b456fa37fca823b370fbd20250a7265e603
Drew Purdy <andrewmp1@gmail.com>
2013-05-28 15:29:40 -0700

    Initial commit of Spree Outlet example app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpreeOutletExample::Application.config.secret_token = '2cf3b3f429c886f9fbd5b5ee32051ac6c6b26fc5fc7bb92bb7cfb1843989c0adb2ca59361d514aa45ffd4576a5da6cd9b52a669075c69d408daa696c9b85dfbc'
